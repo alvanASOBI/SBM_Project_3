6debb47f7bb075189972aac2fe9b96dae937aa8b
Akshi4162 <akshi1246@gmail.com>
2024-05-08 16:03:56 +0800

    included title field in addforum page

--- assets/js/addforum.js
+++ assets/js/addforum.js
@@ -3,15 +3,17 @@ document.getElementById('postForm').addEventListener('submit', function(event) {
 
     var username = document.getElementById('username').value;
     var postContent = document.getElementById('postContent').value;
+    var postTitle = document.getElementById('postTitle').value;
 
     if (username && postContent) {
         var post = document.createElement('div');
         post.className = 'post';
-        post.innerHTML = '<h3>' + username + '</h3><p>' + postContent + '</p>';
+        post.innerHTML = '<h1>' + postTitle + '</h1>' + '<h3>' + username + '</h3><p>' + postContent + '</p>';
         document.getElementById('posts').appendChild(post);
 
         document.getElementById('username').value = '';
         document.getElementById('postContent').value = '';
+        document.getElementById('postTitle').value = '';
     } else {
         alert('Please fill in all fields.');
     }
