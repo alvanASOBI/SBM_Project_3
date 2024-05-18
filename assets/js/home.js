3214e872991bb70be04678034b4b034348942cf2
alvanASOBI <149999395+alvanASOBI@users.noreply.github.com>
2024-05-18 22:43:45 +0800

    updated the home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,22 @@
-document.getElementById('profile-item').addEventListener('click', function() {
+document.getElementById('profile-item').addEventListener('click', function () {
     document.getElementById('bottom-drawer').classList.add('active');
 });
 
-document.getElementById('close-btn').addEventListener('click', function() {
+document.getElementById('close-btn').addEventListener('click', function () {
     document.getElementById('bottom-drawer').classList.remove('active');
+});
+
+document.getElementById('profile-item').addEventListener('click', function () {
+    document.getElementById('bottom-drawer').classList.add('active');
+    document.getElementById('overlay').classList.add('active');
+});
+
+document.getElementById('close-btn').addEventListener('click', function () {
+    document.getElementById('bottom-drawer').classList.remove('active');
+    document.getElementById('overlay').classList.remove('active');
+});
+
+document.getElementById('overlay').addEventListener('click', function () {
+    document.getElementById('bottom-drawer').classList.remove('active');
+    document.getElementById('overlay').classList.remove('active');
 });
