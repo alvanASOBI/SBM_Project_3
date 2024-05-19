385cd3b3f2578f3b63aa48d77b91f09bc416891a
alvanASOBI <149999395+alvanASOBI@users.noreply.github.com>
2024-05-19 13:27:41 +0800

    fixed nav issue

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-document.getElementById('profile-item').addEventListener('click', function () {
+document.getElementById('nav-profile').addEventListener('click', function () {
     document.getElementById('bottom-drawer').classList.add('active');
 });
 
@@ -24,7 +24,7 @@ document.getElementById('close-btn').addEventListener('click', function () {
     document.getElementById('bottom-drawer').classList.remove('active');
 });
 
-document.getElementById('profile-item').addEventListener('click', function () {
+document.getElementById('nav-profile').addEventListener('click', function () {
     document.getElementById('bottom-drawer').classList.add('active');
     document.getElementById('overlay').classList.add('active');
 });
