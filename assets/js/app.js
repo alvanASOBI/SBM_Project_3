3c15d7066cb32a90955a4d47f13e149f3982a162
alvanASOBI <149999395+alvanASOBI@users.noreply.github.com>
2024-05-30 11:39:47 +0800

    fixed app.js bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,17 @@
+console.log('Script loaded');
+
 if ('serviceWorker' in navigator) {
+    console.log('Service Worker supported');
+
     navigator.serviceWorker.register('/service-worker.js')
-        .then((reg) => console.log('service worker registered', reg))
-        .catch((err) => console.log('service worker not registered', err));
+        .then((reg) => {
+            console.log('Service Worker registration attempt');
+            console.log('service worker registered', reg);
+        })
+        .catch((err) => {
+            console.log('Service Worker registration attempt');
+            console.log('service worker not registered', err);
+        });
+} else {
+    console.log('Service Worker not supported in this browser');
 }
