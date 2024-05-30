97b9a7f1d3f1a1b55c6399ed8fef31549e9e96c7
isucatlol <lxw145509@gmail.com>
2024-05-30 10:16:40 +0800

    New Home Page

--- assets/js/harish.js
+++ assets/js/harish.js
@@ -1,11 +1,35 @@
+// let currentBanner = 0;
+// const banners = document.querySelectorAll('.banner');
+// const totalBanners = banners.length;
+
+// function showNextBanner() {
+//     banners[currentBanner].style.transform = `translateX(-${currentBanner * 100}%)`;
+//     currentBanner = (currentBanner + 1) % totalBanners;
+//     banners[currentBanner].style.transform = `translateX(-${currentBanner * 100}%)`;
+// }
+
+// setInterval(showNextBanner, 4000);
+
 let currentBanner = 0;
 const banners = document.querySelectorAll('.banner');
 const totalBanners = banners.length;
 
 function showNextBanner() {
-    banners[currentBanner].style.transform = `translateX(-${currentBanner * 100}%)`;
+    banners.forEach((banner, index) => {
+        banner.style.transform = `translateX(-${currentBanner * 100}%)`;
+    });
+
     currentBanner = (currentBanner + 1) % totalBanners;
-    banners[currentBanner].style.transform = `translateX(-${currentBanner * 100}%)`;
+
+    banners.forEach((banner, index) => {
+        banner.style.transform = `translateX(-${currentBanner * 100}%)`;
+    });
 }
 
+banners.forEach((banner, index) => {
+    banner.style.transform = `translateX(-${index * 100}%)`;
+});
+
 setInterval(showNextBanner, 4000);
+
+
