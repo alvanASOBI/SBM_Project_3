8629eccc876faf323bdadfc21bf2d8f738f03872
HarishAehesu <aehesuharish123@gmail.com>
2024-07-25 11:47:01 +0800

    home.js updated

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,38 +1,3 @@
-// let currentBanner = 0;
-// const banners = document.querySelectorAll('.banner');
-// const totalBanners = banners.length;
-
-// function showNextBanner() {
-//     banners[currentBanner].style.transform = `translateX(-${currentBanner * 100}%)`;
-//     currentBanner = (currentBanner + 1) % totalBanners;
-//     banners[currentBanner].style.transform = `translateX(-${currentBanner * 100}%)`;
-// }
-
-// setInterval(showNextBanner, 4000);
-
-// let currentBanner = 0;
-// const banners = document.querySelectorAll('.banner');
-// const totalBanners = banners.length;
-
-// function showNextBanner() {
-//     banners.forEach((banner, index) => {
-//         banner.style.transform = `translateX(-${currentBanner * 100}%)`;
-//     });
-
-//     currentBanner = (currentBanner + 1) % totalBanners;
-
-//     banners.forEach((banner, index) => {
-//         banner.style.transform = `translateX(-${currentBanner * 100}%)`;
-//     });
-// }
-
-// banners.forEach((banner, index) => {
-//     banner.style.transform = `translateX(-${index * 100}%)`;
-// });
-
-// setInterval(showNextBanner, 4000);
-
-// Import necessary Firestore functionalities if not already imported
 import { db, collection, getDocs } from "../src/components/firebase_configuration.js";
 
 // Function to fetch and display baby profiles
